e516bced23da6280462586a836b79adbab647e9d
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-13 03:03:29 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,31 @@
+class TicTacToe
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
+  attr_accessor :board
+
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
+
+  def display_board2
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+  def play
+  end
+  
+end
