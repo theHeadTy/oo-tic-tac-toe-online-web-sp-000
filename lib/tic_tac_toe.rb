8ad90e37ca504306c0184e2abaa7cd6238f60ffb
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-13 03:05:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,6 +25,16 @@ class TicTacToe
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
   
+  def display_board
+    puts <<~HEREDOC
+      #{board[0]} | #{board[1} | #{board[2}
+      
+      #{board[3]} | #{board[4]} | #{board[5]}
+      
+      #{board[6]} | #{board[7]} | #{board[8]}
+    
+    HEREDOC
+  
   def play
   end
   
